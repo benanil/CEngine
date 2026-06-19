13e987c29bb9e8d38057262ec16e94fe3086de8e
Anılcan Gülkaya <anilcangulkaya7@gmail.com>
2026-06-19 14:53:20 +0300

    remove unused function

--- Source/FileSystem.c
+++ Source/FileSystem.c
@@ -609,14 +609,6 @@ static bool IsAbsolutePath(const char* path)
     return false;
 }
 
-static int StrLenSafe(const char* s)
-{
-    int len = 0;
-    while (s && s[len])
-        ++len;
-    return len;
-}
-
 static void RemoveTrailingSeparator(char* buffer, int* length)
 {
     while (*length > 0 && IsPathSeparator(buffer[*length - 1]))
